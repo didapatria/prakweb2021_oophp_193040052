8605a466d18b4ccfa6a90697acad3791494a042b
didapatria <didapatria3@gmail.com>
2021-09-23 08:11:24 +0700

    Menyelesaikan video OOP DASAR pada PHP #7 - Inheritance (Bagian 2)

---
+++ inheritance.php
@@ -0,0 +1,80 @@
+<?php
+
+class Produk
+{
+  // property
+  public $judul,
+    $penulis,
+    $penerbit,
+    $harga,
+    $jmlHalaman,
+    $waktuMain;
+
+  // constructor
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+    $this->jmlHalaman = $jmlHalaman;
+    $this->waktuMain = $waktuMain;
+    // kegunaan this untuk mengambil isi property yang ada pada class yang bersangkutan ketika dibuat instansi
+    // sedangkan pada parameter itu function atau method merupakan variabel lokal
+  }
+
+  // method
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+    // kegunaan this untuk mengambil isi property yang ada pada class yang bersangkutan ketika dibuat instansi
+  }
+
+  // method
+  public function getInfoProduk()
+  {
+    $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+    return $str;
+  }
+}
+
+// inheritance
+class Komik extends Produk
+{
+  // method
+  public function getInfoProduk()
+  {
+    $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+    return $str;
+  }
+}
+
+// inheritance
+class Game extends Produk
+{
+  // method
+  public function getInfoProduk()
+  {
+    $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
+    return $str;
+  }
+}
+
+// object type
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
+// membuat object instance dari class
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
